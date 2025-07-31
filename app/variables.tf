0aa09089912f80f0fd1e8e631bde8e690a82b7e6
maolopez <maurizio.school@gmail.com>
2025-07-31 13:02:36 -0400

    terraform apply failed again

--- app/variables.tf
+++ app/variables.tf
@@ -1,5 +1,5 @@
 locals {
-  random_suffix = "${substr("abcdefghijklmnopqrstuvwxyz", element(split("", "abcdefghijklmnopqrstuvwxyz"), 0), 1)}${random_string.suffix.result}"
+  random_suffix = "${substr("abcdefghijklmnopqrstuvwxyz", 0, 1)}${random_string.suffix.result}"
   region              = "us-east-1"
   awsaccount          = "271271282869"
   bot_name            = "weather_small_talk_${local.random_suffix}"
