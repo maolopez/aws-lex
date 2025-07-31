97e0ca37f01380b936afb25020ce4a2f37a60689
maolopez <maurizio.school@gmail.com>
2025-07-31 16:05:53 -0400

    moving to lex V2

--- app/main.tf
+++ app/main.tf
@@ -8,8 +8,9 @@ resource "random_string" "suffix" {
 module "lex" {
   source = "../modules/lex"
 
-  region              = local.region
-  bot_name            = local.bot_name
-  iddle_time          = local.iddle_time
-  detect_sentiment    = local.detect_sentiment
+  region         = local.region
+  bot_name       = local.bot_name
+  lex_bot_role   = local.lex_bot_role
+  lex_bot_policy = local.lex_bot_policy
+  slot_type_name = local.slot_type_name
 }
--- app/variables.tf
+++ app/variables.tf
@@ -1,9 +1,10 @@
 locals {
-  random_suffix = "${substr("abcdefghijklmnopqrstuvwxyz", 0, 1)}${random_string.suffix.result}"
-  region              = "us-east-1"
-  awsaccount          = "271271282869"
-  bot_name            = "WeatherSmallTalk_${local.random_suffix}"  #expected value of name to match regular expression "^([A-Za-z]_?)+$"
-  iddle_time          = 600
-  detect_sentiment    = true
+  random_suffix  = "${substr("abcdefghijklmnopqrstuvwxyz", 0, 1)}${random_string.suffix.result}"
+  region         = "us-east-1"
+  awsaccount     = "271271282869"
+  bot_name       = "WeatherSmallTalk_${local.random_suffix}" #expected value of name to match regular expression "^([A-Za-z]_?)+$"
+  lex_bot_role   = "lbrole-${random_string.suffix.result}"
+  lex_bot_policy = "lbpolicy-${random_string.suffix.result}"
+  slot_type_name = "stweather_${local.random_suffix}"
 
 }
