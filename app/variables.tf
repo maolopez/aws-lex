bbb20366ea1f1f642dc47aac42206c92eee9a491
maolopez <maurizio.school@gmail.com>
2025-07-31 13:05:24 -0400

    terraform apply failed again

--- app/variables.tf
+++ app/variables.tf
@@ -2,7 +2,7 @@ locals {
   random_suffix = "${substr("abcdefghijklmnopqrstuvwxyz", 0, 1)}${random_string.suffix.result}"
   region              = "us-east-1"
   awsaccount          = "271271282869"
-  bot_name            = "weather_small_talk_${local.random_suffix}"
+  bot_name            = "WeatherSmallTalk_${local.random_suffix}"  #expected value of name to match regular expression "^([A-Za-z]_?)+$"
   iddle_time          = 600
   detect_sentiment    = true
 
