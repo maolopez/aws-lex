3989858175a2654d4f845610bd5d986f2381a9d6
maolopez <maurizio.school@gmail.com>
2025-07-31 11:25:47 -0400

    first terraform validate

--- app/main.tf
+++ app/main.tf
@@ -8,7 +8,6 @@ module "lex" {
   source = "../modules/lex"
 
   region              = local.region
-  bucket_name_samples = local.bucket_name_samples
   bot_name            = local.bot_name
   iddle_time          = local.iddle_time
   detect_sentiment    = local.detect_sentiment
--- app/variables.tf
+++ app/variables.tf
@@ -1,7 +1,6 @@
 locals {
   region              = "us-east-1"
   awsaccount          = "271271282869"
-  bucket_name_samples = "bucket-samples-${random_string.suffix.result}"
   bot_name            = "weather_small_talk_${random_string.suffix.result}"
   iddle_time          = 600
   detect_sentiment    = true
